1688680b26176787bbd34b622fd6926992647b87
Adil Gondal <adil.gondal@yale.edu>
2020-05-22 12:21:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word (str)
   array = str.split(" ")
-  new_array = []
-  array.each do |word|
-    new_array.push(word.reverse())
+  array.collect do |word|
+    word.reverse()
   end
   new_array.join(" ")
 end
